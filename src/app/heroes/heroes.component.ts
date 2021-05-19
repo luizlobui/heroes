67d32ba94f5aa1aaa4bf0ffc40bf3058de36d728
Luiz Lobui <luiz.lobui@hotmail.com>
2021-05-19 17:40:41 -0300

    Parte 6 Json

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,7 +4,6 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
 
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -13,7 +12,7 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -21,6 +20,22 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-}
+      .subscribe(heroes => this.heroes = heroes);
+      }
+    
+      add(name: string): void {
+        name = name.trim();
+        if (!name) {
+          return;
+        }
+        this.heroService.addHero({ name } as Hero)
+          .subscribe(hero => {
+            this.heroes.push(hero);
+          });
+      }
+    
+      delete(hero: Hero): void {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        this.heroService.deleteHero(hero.id).subscribe();
+      }
+    }
